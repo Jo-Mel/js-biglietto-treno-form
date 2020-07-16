8feed541eda698aab137c2304812e298eaad17b7
Jona M <jonameleqi@gmail.com>
2020-07-16 23:39:06 +0200

    ok

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// Creiamo un finto biglietto del treno con:
+// Nome passeggero
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza
+// Prezzo calcolato
+// Categoria selezionata dall'utente
+// Aggiungiamo una piccola animazione al click su "Crea" e 
+//"Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
+
+document.getElementById('genera').addEventListener('click', function(){ // al click del bottone 'genera' eseguo la funzione
+    document.getElementById('tck').classList.remove('hidden');
+
+   
+    var nomeCognome = document.getElementById('nome-pass').value;   
+    var km = document.getElementById('km').value;                     // ottengo i valori inseriti nel form
+    var age = document.getElementById('age-range').value;
+
+    document.getElementById('tck-nome-pass').innerHTML = nomeCognome;
+
+    var carrozza = Math.floor(Math.random()* 10);
+    document.getElementById('tck-carrozza').innerHTML = carrozza;
+
+    var codice = Math.floor(Math.random()* (99999 - 90000 + 1) + 90000);
+    document.getElementById('tck-cod-cp').innerHTML = codice;
+
+    var prezzo = 0.21 * km;
+    prezzo = prezzo.toFixed(2);
+    var offerta = 'Prezzo Standard';
+
+    if (age == 'min') {
+        prezzo *= 0.8;
+        prezzo = prezzo.toFixed(2);
+        document.getElementById('tck-prezzo').innerHTML = '€ ' + prezzo;
+        offerta = 'Sconto minori di 18 anni';
+        document.getElementById('tck-sconto').innerHTML = offerta;
+    } else if (age == 'over'){
+        prezzo *= 0.6;
+        prezzo = prezzo.toFixed(2);
+        document.getElementById('tck-prezzo').innerHTML = '€ ' + prezzo;
+        offerta = 'Sconto over 65';
+        document.getElementById('tck-sconto').innerHTML = offerta;
+    } else {
+        document.getElementById('tck-prezzo').innerHTML = '€ ' + prezzo;
+        document.getElementById('tck-sconto').innerHTML = offerta;
+    }
+
+});
+
+document.getElementById('annulla').addEventListener('click', function(){
+    location.reload();
+});
+
