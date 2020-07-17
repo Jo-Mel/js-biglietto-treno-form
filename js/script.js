fed5a7d220a56c14ea7b6d0ce1a7d1c7447d3c4c
Jona M <jonameleqi@gmail.com>
2020-07-17 18:01:06 +0200

    piccole modifiche

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // Nome passeggero
 // Codice treno (numero casuale tra 90000 e 100000 escluso)
 // Numero carrozza
-// Prezzo calcolato
+// Prezzo calcolato   
 // Categoria selezionata dall'utente
 // Aggiungiamo una piccola animazione al click su "Crea" e 
 //"Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
@@ -17,11 +17,19 @@ document.getElementById('genera').addEventListener('click', function(){ // al cl
 
     document.getElementById('tck-nome-pass').innerHTML = nomeCognome;
 
-    var carrozza = Math.floor(Math.random()* 10);                      // genero i numeri random per carrozza e codice CP
+    // genero la carrozza e il codice cp richiamando la funzione
+    var carrozza = generaRandom(1,10); 
+    var codice = generaRandom(90000,100000);
+    //visualizzo 
     document.getElementById('tck-carrozza').innerHTML = carrozza;
-
-    var codice = Math.floor(Math.random()* (99999 - 90000 + 1) + 90000);
     document.getElementById('tck-cod-cp').innerHTML = codice;
+
+
+    // var carrozza = Math.floor(Math.random()* 10);                  // genero i numeri random per carrozza e codice CP
+    // document.getElementById('tck-carrozza').innerHTML = carrozza;
+
+    // var codice = Math.floor(Math.random()* (99999 - 90000 + 1) + 90000);
+    // document.getElementById('tck-cod-cp').innerHTML = codice;
 
     var prezzo = 0.21 * km; // calcolo il prezzo standard
     prezzo = prezzo.toFixed(2);
@@ -44,7 +52,12 @@ document.getElementById('genera').addEventListener('click', function(){ // al cl
         document.getElementById('tck-sconto').innerHTML = offerta;
     }
 
+    
 });
+
+function generaRandom(min,max){ // la funzione per generare i numeri random
+    return Math.floor(Math.random()*(max -min) + min);
+}
 
 document.getElementById('annulla').addEventListener('click', function(){ //funzione per il reset dei dati al click del bottone 'annulla'
     location.reload();
